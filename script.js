51b2e9348c169ebf261939a7bca2d8c0f303a684
Heo <sciencemath177@gmail.com>
2023-11-07 19:47:43 +0900

    other solution

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-for(let i = 2; i <= 9; i++) {
-  document.write(i + '단');
-  document.write('<br>');
-  for(let j = 1; j <= 9; j++) {
-    document.write(i + ' * ' + j + ' = ' + (i*j));
-    document.write('<br>');
+// for(let i = 2; i <= 9; i++) {
+//   document.write(i + '단');
+//   document.write('<br>');
+//   for(let j = 1; j <= 9; j++) {
+//     document.write(i + ' * ' + j + ' = ' + (i*j));
+//     document.write('<br>');
+//   }
+// }
+
+
+
+const multi = function table(){for (let i=2; i<10; i++){
+  for(let j=1; j<10; j++){
+    console.log(`${i} * ${j} = ${i * j}`)
   }
-}
+  
+}}
+
